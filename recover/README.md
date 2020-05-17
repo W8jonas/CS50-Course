f8d8186d251346309a4325b892ed613f77c9145c
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-05-16 21:03:24 -0300

    recover finalizado

--- recover/README.md
+++ recover/README.md
@@ -1,1 +1,7 @@
 recover, c
+
+Este programa é capaz de recuperar imagens jpg de um arquivo .raw.
+
+Para usar o programa mario basta executa-lo e entrar com o arquivo.raw como no exemplo:
+
+$ ./recover card.raw
--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define BLOCK_SIZE 512
+
+typedef uint8_t BYTE;
+
+// start of jpeg file 0xff 0xd8 0xff 0xe?
 
 int main(int argc, char *argv[])
 {
+    // Check for invalid usage
+    if (argc > 2)
+    {
+        printf("Usage: recover nameOfCard\n");
+        return 1;
+    }
 
+    char *infile = argv[1];
+
+    // open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open file");
+        return 1;
+    }
+
+    BYTE buffer[512];
+    int imageCount = 0;
+
+    char filename[8];
+    FILE *outFile = NULL;
+
+    while (true)
+    {
+        // read a block of memory
+        size_t bytesRead = fread(buffer, sizeof(BYTE), BLOCK_SIZE, inptr);
+
+        if (bytesRead == 0 && feof(inptr))
+        {
+            break;
+        }
+
+        // get a jpeg header
+        bool containsJpegHeader = buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+
+
+        if (containsJpegHeader && outFile != NULL)
+        {
+            fclose(outFile);
+            imageCount++;
+        }
+
+        if (containsJpegHeader)
+        {
+            sprintf(filename, "%03i.jpg", imageCount);
+            outFile = fopen(filename, "w");
+        }
+
+        if (outFile != NULL)
+        {
+            fwrite(buffer, sizeof(BYTE), bytesRead, outFile);
+        }
+
+    }
+
+    // close files
+    fclose(outFile);
+    fclose(inptr);
+
+    // success
+    return 0;
 }
