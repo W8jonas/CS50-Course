ff4c6af7975659d1d997440fdd58cc101d405294
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-07 21:18:09 -0300

    Inicio codigo DNA pset6

--- pset6 - Python 🐍🧡/DNA/dna.py
+++ pset6 - Python 🐍🧡/DNA/dna.py
@@ -1,10 +1,16 @@
 from sys import argv
+import csv
 
 while True:
     if (len(argv) != 2):
         print("Error. Usage: databases/?.csv sequences/?.txt ")
         break
-    
 
+    print(argv[1])
 
+    with open(argv[1], 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            print(row)
+        break
 
