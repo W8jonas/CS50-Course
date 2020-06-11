4763f6509bd041a89b2458cb06695e5ecba2e7d6
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-11 16:45:57 -0300

    Feito exercicios do 6 ao 10 movies em pset7 - SQL

---
+++ pset7 - SQL/movies/10.sql
@@ -0,0 +1,5 @@
+SELECT name FROM people
+JOIN directors ON people.id = directors.person_id
+JOIN movies ON movies.id = directors.movie_id
+JOIN ratings ON ratings.movie_id = movies.id
+WHERE ratings.rating > 9;
