a17e1b36ad02230077d5ebcde348d62e3108b472
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-13 11:30:41 -0300

    corrigido erro exercicio num 10 movies em pset7 - SQL

--- pset7 - SQL/movies/10.sql
+++ pset7 - SQL/movies/10.sql
@@ -2,4 +2,4 @@ SELECT name FROM people
 JOIN directors ON people.id = directors.person_id
 JOIN movies ON movies.id = directors.movie_id
 JOIN ratings ON ratings.movie_id = movies.id
-WHERE ratings.rating > 9;
+WHERE ratings.rating >= 9;
