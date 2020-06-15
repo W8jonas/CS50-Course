77968390e4277b8c8b09e28d20e2d4d0575c5656
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-15 16:59:58 -0300

    feito roster.py em houses pset7 - SQL

--- pset7 - SQL/houses/roster.py
+++ pset7 - SQL/houses/roster.py
@@ -1,1 +1,23 @@
-# TODO
+from sys import exit, argv
+from csv import reader, DictReader
+from cs50 import SQL
+
+if (len(argv) != 2):
+    print("Error.  Usage: python roster.py Gryffindor ")
+    exit()
+
+db = SQL("sqlite:///students.db")
+
+students = db.execute("SELECT * FROM students WHERE house = (?) ORDER BY last", argv[1])
+
+for student in students:
+
+    first = student['first']
+    middle = student['middle']
+    last = student['last']
+    birth = student['birth']
+
+    if(middle):
+        print(f"{first} {middle} {last}, born {birth}")
+    else:
+        print(f"{first} {last}, born {birth}")
