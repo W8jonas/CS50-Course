141e5bc1d4c786f4bf054e750001c946b8a79fc9
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-15 16:37:33 -0300

    feito import.py em houses pset7 - SQL

--- pset7 - SQL/houses/import.py
+++ pset7 - SQL/houses/import.py
@@ -1,1 +1,29 @@
-# TODO
+from sys import exit, argv
+from csv import reader, DictReader
+from cs50 import SQL
+
+if (len(argv) != 2):
+    print("Error. Usage: characters.csv ")
+    exit()
+
+db = SQL("sqlite:///students.db")
+
+with open(argv[1]) as csv_file:
+    csv_rows = DictReader(csv_file)
+    for row in csv_rows:
+
+        name = row['name'].split()
+
+        if(len(name) == 2):
+            first = name[0]
+            middle = None
+            last = name[1]
+        else:
+            first = name[0]
+            middle = name[1]
+            last = name[2]
+
+        house = row['house']
+        birth = row['birth']
+
+        db.execute('INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)', first, middle, last, house, birth)
