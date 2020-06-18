010f8db1d495ef40d6c6f08f108b19c5fa100d76
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-17 23:38:34 -0300

    Pong aula 0 - Hello Pong pset8-games

--- pset8_games_❤😍/pong/main.lua
+++ pset8_games_❤😍/pong/main.lua
@@ -1,4 +1,14 @@
+WINDOW_WIDTH = 800
+WINDOW_HEIGTH = 600
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGTH, {
+        fullscreen = false,
+        vsync = true,
+        resizable = false
+    })
+end
 
 function love.draw()
-    love.graphics.print("Hello World guys", 400, 300)
+    love.graphics.printf("Hello pong", 0, WINDOW_HEIGTH / 2 - 6, WINDOW_WIDTH, 'center')
 end
