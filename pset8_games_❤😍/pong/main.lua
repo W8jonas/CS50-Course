5a053673d44338cb7d9e42ea8da279935c2a3ae3
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-18 21:49:47 -0300

    Aula Pong 1 tracks - games

--- pset8_games_❤😍/pong/main.lua
+++ pset8_games_❤😍/pong/main.lua
@@ -1,14 +1,31 @@
 WINDOW_WIDTH = 800
 WINDOW_HEIGTH = 600
 
+VIRTUAL_WIDTH = 400
+VIRTUAL_HEIGTH = 200
+
+push = require 'push'
+
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGTH, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGTH, WINDOW_WIDTH, WINDOW_HEIGTH, {
         fullscreen = false,
         vsync = true,
         resizable = false
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
-    love.graphics.printf("Hello pong", 0, WINDOW_HEIGTH / 2 - 6, WINDOW_WIDTH, 'center')
+    push:apply('start')
+
+    love.graphics.printf("Hello pong", 0, VIRTUAL_HEIGTH / 2 - 6, VIRTUAL_WIDTH, 'center')
+    
+    push:apply('end')
 end
