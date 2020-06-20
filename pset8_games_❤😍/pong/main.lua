8048d30cf4112417fc2dd5a4993c0a403b81acdf
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-19 22:39:41 -0300

    Adicionando movimento nas raquetes aula 3 tracks - games - Pong

--- pset8_games_❤😍/pong/main.lua
+++ pset8_games_❤😍/pong/main.lua
@@ -4,8 +4,9 @@ WINDOW_HEIGTH = 600
 VIRTUAL_WIDTH = 400
 VIRTUAL_HEIGTH = 200
 
-push = require 'push'
+PADDLE_SPEED = 200
 
+push = require 'push'
 
 
 function love.load()
@@ -13,7 +14,7 @@ function love.load()
     
     smallFont = love.graphics.newFont('font.ttf', 8)
 
-    love.graphics.setFont(smallFont)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGTH, WINDOW_WIDTH, WINDOW_HEIGTH, {
         fullscreen = false,
@@ -21,7 +22,35 @@ function love.load()
         resizable = false
     })
 
+    -- Variáveis globais
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGTH - 40
+
 end
+
+
+function love.update(dt)
+
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+
+
+end
+
 
 function love.keypressed(key)
     if key == 'escape' then
@@ -35,18 +64,24 @@ function love.draw()
     -- Colocar cor de fundo
     love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 
-    -- Desenha bola no meio da tela
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2-2, VIRTUAL_HEIGTH/2-2, 5, 5)
-   
-    -- Desenha bola no meio da tela
-    love.graphics.rectangle('fill', 5, 20, 5, 20)
-    
-    -- Desenha bola no meio da tela
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGTH - 40, 5, 20)
-
-
+    love.graphics.setFont(smallFont)
     -- Escreve Hello pong na tela
     love.graphics.printf("Hello pong", 0, 20, VIRTUAL_WIDTH, 'center')
     
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(player1Score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGTH/3 )
+    love.graphics.print(player2Score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGTH/3 )
+
+    -- Desenha bola no meio da tela
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2-2, VIRTUAL_HEIGTH/2-2, 5, 5)
+   
+    -- Desenha uma raquete na esquerda
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    
+    -- Desenha uma raquete na direita
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+
+
+
     push:apply('end')
 end
