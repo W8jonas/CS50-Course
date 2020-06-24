e254941bb294d308b96740f2854ef412a35a4744
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-06-24 00:50:58 -0300

    Adicionando audio ao jogo, track - games - Pong

--- pset8_games_❤😍/pong/main.lua
+++ pset8_games_❤😍/pong/main.lua
@@ -22,10 +22,14 @@ function love.load()
     love.window.setTitle('Pong')
 
     smallFont = love.graphics.newFont('font.ttf', 8)
-
     scoreFont = love.graphics.newFont('font.ttf', 32)
-
     victoryFont = love.graphics.newFont('font.ttf', 24)
+
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['point_scored'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGTH, WINDOW_WIDTH, WINDOW_HEIGTH, {
         fullscreen = false,
@@ -63,6 +67,7 @@ function love.update(dt)
 
         if ball.x <= 0 then
             player2Score = player2Score + 1 
+            sounds['point_scored']:play()
             servingPlayer = 1
             ball:reset()
             ball.dx = 100            
@@ -78,6 +83,7 @@ function love.update(dt)
 
         if ball.x >= VIRTUAL_WIDTH - 4 then
             player1Score = player1Score + 1 
+            sounds['point_scored']:play()
             servingPlayer = 2
             ball:reset()
             ball.dx = -100            
@@ -95,6 +101,8 @@ function love.update(dt)
             ball.dx = -ball.dx * 1.03
             ball.x = paddle1.x + 5
 
+            sounds['paddle_hit']:play()
+
             if ball.dy < 0 then
                 ball.dy = -math.random(10, 150)
             else
@@ -103,17 +111,28 @@ function love.update(dt)
         end
     
         if ball:Collides(paddle2) then
-            ball.dx = -ball.dx
+            ball.dx = -ball.dx * 1.03
+            ball.x = paddle2.x - 4
+
+            sounds['paddle_hit']:play()
+
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
         end
     
         if ball.y <= 0 then
             ball.dy = - ball.dy
             ball.y = 0
+            sounds['wall_hit']:play()
         end
     
         if ball.y > VIRTUAL_HEIGTH then
             ball.dy = - ball.dy
             ball.y = VIRTUAL_HEIGTH - 4
+            sounds['wall_hit']:play()
         end
         
         -- Movimentação do jogador 1
@@ -182,7 +201,7 @@ function love.draw()
     elseif gameState == 'victory' then
         love.graphics.setFont(victoryFont)
         love.graphics.printf('Player ' .. tostring(winningPlayer) .. " wins!", 0, 10, VIRTUAL_WIDTH, 'center')
-        love.graphics.setFont(scoreFont)
+        love.graphics.setFont(smallFont)
         love.graphics.printf('Press Enter to serve!', 0, 42, VIRTUAL_WIDTH, 'center')
     end
     
