a079a29902be5b8fbc14eeef4850060981e8bc9a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-07-16 19:56:57 -0300

    inicio main.lua projeto final

---
+++ pset9_project/atari_breakout/main.lua
@@ -0,0 +1,28 @@
+WINDOW_WIDTH = 800
+WINDOW_HEIGTH = 600
+
+VIRTUAL_WIDTH = 400
+VIRTUAL_HEIGTH = 200
+
+
+
+
+function love.load()
+
+end
+
+
+function love.update(dt)
+
+end
+
+
+function love.keypressed(key)
+
+end
+
+
+function love.draw()
+
+end
+
