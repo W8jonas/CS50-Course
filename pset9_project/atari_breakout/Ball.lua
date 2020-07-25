57c6a1a53a3ea9d2d3794c8a9b5026f0e37ef21a
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2020-07-25 15:39:33 -0300

    Colocando colisoes da bolinha pelas bordas do mapa

--- pset9_project/atari_breakout/Ball.lua
+++ pset9_project/atari_breakout/Ball.lua
@@ -41,6 +41,26 @@ function Ball:update(dt)
     self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
 
+    if self.y <= 0 then
+        self.dy = - self.dy
+        self.y = 0 * dt
+    end
+
+    if self.x <= 0 then
+        self.dx = - self.dx
+        self.x = 0 * dt
+    end
+
+    if self.x > WINDOW_WIDTH then
+        self.dx = - self.dx
+        self.x = WINDOW_WIDTH - self.x * dt
+    end
+
+    -- if ball.y > WINDOW_HEIGTH then
+    --     ball.dy = - ball.dy
+    --     ball.y = WINDOW_HEIGTH - 4
+    -- end
+
 end
 
 
